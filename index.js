bd6a3a5175007de0f40a696d0220fae329153486
Abhijeet Dewtarse <121091011+Abhidewtarse@users.noreply.github.com>
2024-07-23 17:17:40 +0530

    Update2 index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyparser = require("body-parser");
 var upload = require("express-fileupload");
 var session = require("express-session");
+const PORT = process.env.PORT || 3030;
 
 
 var user_route = require("./Routes/user_route");
@@ -20,4 +21,6 @@ app.use(session(
 app.use("/", user_route);
 app.use("/admin", admin_route);
 
-app.listen(1001);
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
